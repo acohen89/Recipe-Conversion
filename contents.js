023b79320e2de7814aef8bda305f043f52c1188d
Adam Cohen <acohen89@binghamton.edu>
2020-12-31 12:33:37 -0800

    Created findmetric function and started creating posisble nums

--- contents.js
+++ contents.js
@@ -2,6 +2,11 @@ console.log("Recipe Chrome Extension Loaded");
 let cupToGrams  = {"AP Flour": 130.0, "Bread Flour": 135.0, "WW Flour": 128.0, "Rye Flour": 102.0, "Water": 240.0, "Sugar": 200.0, "Raw Sugar": 250.0, "Brown Sugar": 220.0,
 "Confectioners Sugar": 125.0, "Milk": 242.0, "Cocoa Powder": 85.0, "Oil": 242.0, "Chocolate Chunks": 140.0, "Chocolate Chips": 170.0, "Nuts": 110.0, "Raisins": 155.0, "Oats": 105.0, "Heavy Cream": 225.0, "Buttermilk": 225.0, "Yogurt": 225.0, "Sour Cream": 225.0, "Peanut Butter": 250, "Rice": 200.0, "Light Corn Syrup": 328.0, "Dark Corn Syrup": 328.0, "Corn Syrup": 328.0
  }
+let metricDict = {"Tsp": "Teaspoon", "tsp": "Teaspoon", "TSP": "Teaspoon", "teaspoon": "Teaspoon", "Teaspoon": "Teaspoon", "TeaSpoon": "Teaspoon", "TSPs": "Teaspoon", "Tsps": "Teaspoon", "Teaspoons": "Teaspoon", "teaspoons": "Teaspoon", "Teaspoons": "Teaspoon",
+"Tbs": "Tablespoon", "tbs": "Tablespoon", "TBS": "Tablespoon", "tablespoon": "Tablespoon", "Tablespoon": "Tablespoon", "TableSpoon": "Tablespoon", "TBSs": "Tablespoon", "Tbss": "Tablespoon", "Tablespoon": "Tablespoon", "tablespoons": "Tablespoon", "Tablespoons": "Tablespoon", 
+
+"Cups": "Cup", "cups": "Cup", "Cup": "Cup", "cup": "Cup", "CUPS": "Cup", "CUP": "Cup", "c": "Cup", "C": "Cup"}
+let possibleNums = {"1/2": 0.5, "1/3": 0.333, "1/4": 0.25, "1/8": 0.125, "2/3": 0.666, "3/4": 0.75, "3/8": 0.375, "5/8": 0.625, "7/8": 0.875}
 let teaspoonToGrams = {"Salt": 5.0, "Yeast": 3.1,}
 let tablespoonToGrams = {"Butter": 14.1, "Honey": 21.25, "Cornstarch": 9.375, "Maple Syrup": 20.0}
 let element = Array.prototype.slice.call(document.querySelectorAll("li"));
@@ -9,17 +14,43 @@ for (let i  = 0; i < element.length; i++) {
     // console.log(element[i].textContent);  
     let ingredient = ingredientSearch(element[i].textContent);
     if(dictNotNull(ingredient)) {
-        //console.log(ingredient);
        console.log(element[i].textContent);  
-       element[i].textContent = element[i].textContent.replace(ingredient["Item"], "REPLACED!");
-       element[i].textContent = element[i].textContent.replace(ingredient["Item"].toLowerCase(), "REPLACED!"); // not replacing because one is capital one is lower case 
+       let ogNumandMetric = findNumandMetric(element[i].textContent); 
+       if(ogNumandMetric[0] != "NULL" && ogNumandMetric[1] != "NULL"){
+        element[i].textContent = element[i].textContent.replace(ingredient["Item"], "REPLACED!");
+        element[i].textContent = replace(element[i].textContent, ingredient, ogNumandMetric);
+        element[i].textContent = element[i].textContent.replace(ingredient["Item"].toLowerCase(), "REPLACED!"); // Replaces lowercase words  
+       }
     }
     // console.log(element[i].textContent);
     //let tempArr = element[i].textContent.split(" ");
     // console.log(tempArr);
 }
-// let testArr = ["1 cup salted butter* softened", "1 cup white (granulated) sugar", "1 cup light brown sugar packed", "2 tsp pure vanilla extract", "2 large eggs"]
-// element.textContent = el.textContent.replace("HTML", "test! ");
+
+function replace(ogText, ingredientDict){
+    let ogTextArr = ogText.split(" ");
+    let replacementText = ""; 
+
+
+    return replacementText;
+}
+
+function findNumandMetric(text){
+    let textArr = text.split(" ");
+    let retArr = [0, ""];
+    for(let i = 0; i < textArr.length; i++){
+        if(textArr[i] in metricDict) {
+            // searches for either cups, tablespoons, or teaspoons
+            retArr[1] = metricDict[textArr[i]];
+        }
+        else {
+            return ["NULL", "NULL"]
+        }
+
+
+    }
+}
+
 function ingredientSearch(foodEl){
     let ret = {"Item": "NULL", "Multiple": "NULL", "OG Metric": "NULL" }
     let found = false;
@@ -76,3 +107,5 @@ function dictNotNull(dict){
 // add extracts and vanilla 
 // make a check for sugar functions, determining if it is brown or white
 // add vegetable stocks and broths 
+// eventually add items like vegetables 
+// add liters and gallons eventually? 
